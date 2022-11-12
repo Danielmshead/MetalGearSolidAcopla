4e9f13cd57c63cc968811b491a0216961b15154b
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-11-12 02:38:14 -0300

    Adicionar arquivos de projeto.

---
+++ SolidAcopla/GeraNotaFiscal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidAcopla
+{
+    class GeraNotaFiscal
+    {
+        private IList<IAcaoAposGerarNota> acoes;
+        public GeraNotaFiscal(IList<IAcaoAposGerarNota> acoes)
+        {
+            this.acoes = acoes;
+        }
+
+        public NotaFiscal Gera(Fatura fatura)
+        {
+            double valor = fatura.ValorMensal;
+
+            NotaFiscal nf = new NotaFiscal(valor, ImpostoSimplesSobre0(valor));
+
+            foreach (var acao in acoes)
+            {
+                acao.Executa(nf);
+            }
+
+            return nf;
+        }
+
+        private double ImpostoSimplesSobre0(double valor)
+        {
+            return valor  * 0.06;
+        }
+    }
+}
---
+++ SolidAcopla/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SolidAcopla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailSender emailSender = new EmailSender();
+            NfDao nfDao = new NfDao();
+            IList<IAcaoAposGerarNota> acoes = new List<IAcaoAposGerarNota>();
+            acoes.Add(new EmailSender());
+            acoes.Add(new NfDao());
+            acoes.Add(new Sap());
+            GeraNotaFiscal gNf = new GeraNotaFiscal(acoes);
+
+            Fatura fatura= new Fatura(200, "Daniel MS ");
+
+            gNf.Gera(fatura);
+
+            Console.ReadKey();  
+        }
+    }
+}
